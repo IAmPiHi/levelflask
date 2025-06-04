0ebb99f65131fca3ab485bea3d005dfcfd40c058
PiHi <140247606+IAmPiHi@users.noreply.github.com>
2025-06-04 17:10:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)), debug=os.getenv("FLASK_ENV") == "development")
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)), debug=False)
